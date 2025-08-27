e2045fe42b26d2cbab8ee71429158215c7de21d4
Teresa Spencer <teresalandkamer@gmail.com>
2025-08-27 11:11:38 -0500

    Refactor variable definitions and assignments in main.js for clarity

--- main.js
+++ main.js
@@ -6,27 +6,41 @@ given in the comments.
 *** DO NOT CHANGE any of the code that you are not instructed to. */
 
 // 1) Define a variable named myNumber.
+let myNumber;
 
 // 2) Assign a value of 5 to the variable myNumber.
+myNumber = 5;
 
 // 3) Define a variable named myOtherNumber and assign it a value of 10
+let myOtherNumber = 10;
 
 /* 4) Define a variable named myTotal and 
 assign the product of myNumber and myOtherNumber to it  */
+let myTotal = myNumber * myOtherNumber;
 
 /* 5) Define a variable named myName and assign your name as the value. 
 Define a 2nd variable named myCombo and assign it the result of the variables 
 myName + myNumber  */
+let myName = "Teresa Spencer";
+
+let myCombo = myName + myNumber;
 
 /* 6) Define a variable named myOtherCombo. Set the value of myOtherCombo equal to  
 the variable myNumber + 8 */
+let myOtherCombo = myNumber + 8;
 
 // 7) Assign the remainder of myNumber divided by 5 to a variable named myRemainder
+let myRemainder = myNumber % 5;
 
 /* 8) Assign a value of 1 to a variable named i. 
 On the next line, increment the value of i with a postfix operator */
+let i = 1;
+i++;
 
 /* 9) Assign a value of 5 to a variable named int. 
 On the following line, decrement int using a prefix operator */
+let int = 5;
+--int;
 
 // 10) Assign an equality check of (myNumber === myName) to a variable named myBoolean
+let myBoolean = (myNumber === myName);
